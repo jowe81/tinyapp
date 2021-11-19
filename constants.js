d8db6dbcef70d6f5ce4c3fb98cb47db03a6c6674
Johannes Weber <johannes@drweber.de>
2021-11-19 11:27:07 -0800

    Add URL_DATABASE

--- constants.js
+++ constants.js
@@ -1,5 +1,11 @@
 const constants = {
+
   PORT: 8080,
+
+  URL_DATABASE: {
+    "b2xVn2": "http://www.lighthouselabs.ca",
+    "9sm5xK": "http://www.google.com"
+  },
 };
 
 module.exports = constants;
