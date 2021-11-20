3fe4c4488cf1040034d9142b96c094e21694f33a
Johannes Weber <johannes@drweber.de>
2021-11-19 17:22:01 -0800

    remove URL_DATABASE (back to express_server.js)

--- constants.js
+++ constants.js
@@ -2,10 +2,6 @@ const constants = {
 
   PORT: 8080,
 
-  URL_DATABASE: {
-    "b2xVn2": "http://www.lighthouselabs.ca",
-    "9sm5xK": "http://www.google.com"
-  },
 };
 
 module.exports = constants;
