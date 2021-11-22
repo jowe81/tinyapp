b025daf6d8cc8f1683c9b527574d3917924dee12
Johannes Weber <johannes@drweber.de>
2021-11-21 17:58:32 -0800

    write tests for emailExists

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -1,10 +1,17 @@
+const assert = require('chai').assert;
 const helpers = require('../helpers');
 const database = require('../database.js');
 
-describe("emailExists", () => {
+describe("helpers.emailExists", () => {
 
-  it("returns true when looking for an email address that is already registered", () => {
-    //helpers.emailExists(database.users,"johannes@drweber.de");
+  const testEmail = "example@email.com";
+  it(`returns false when looking for an email address that is not yet registered (${testEmail})`, () => {
+    assert.isFalse(helpers.emailExists(database.users, testEmail));
+  });
+
+  it(`returns true when looking for an email address that is already registered (${testEmail})`, () => {
+    database.addUser(testEmail, "somepassword");
+    assert.isTrue(helpers.emailExists(database.users, testEmail));
   });
 
 });
