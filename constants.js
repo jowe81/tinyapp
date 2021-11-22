3a2eb910f7837499fc02449b5e4564876801630c
Johannes Weber <johannes@drweber.de>
2021-11-21 16:53:44 -0800

    add MIN_PASSWORD_LENGTH, ID_LENGTH

--- constants.js
+++ constants.js
@@ -2,6 +2,11 @@ const constants = {
 
   PORT: 8080,
 
+  //Minimum password length required for user registration
+  MIN_PASSWORD_LENGTH: 6,
+
+  //Length for auto-generated random IDs
+  ID_LENGTH: 6,
 };
 
 module.exports = constants;
