5dc32f646571d5ab061be2b5a1288cb9dc7bb826
Johannes Weber <johannes@drweber.de>
2021-11-21 17:28:36 -0800

    add database.js for url and user data

---
+++ database.js
@@ -0,0 +1,16 @@
+//database.js: exports the users and urls objects
+
+const urls = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+const users = {
+  "rndmID": {
+    id: "rndmID",
+    email: "johannes@drweber.de",
+    password: "password"
+  }
+};
+
+module.exports = { urls, users };
