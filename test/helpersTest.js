a00f4c4653545599229084da445ce6e78b720797
Johannes Weber <johannes@drweber.de>
2021-11-21 17:31:22 -0800

    add helpersTest.js

---
+++ test/helpersTest.js
@@ -0,0 +1,10 @@
+const helpers = require('../helpers');
+const database = require('../database.js');
+
+describe("emailExists", () => {
+
+  it("returns true when looking for an email address that is already registered", () => {
+    //helpers.emailExists(database.users,"johannes@drweber.de");
+  });
+
+});
