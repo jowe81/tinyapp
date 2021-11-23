c383b6fda673f7036d161ed781390c4467762c02
Johannes Weber <johannes@drweber.de>
2021-11-22 18:21:09 -0800

    clean up

--- middleware/flash.js
+++ middleware/flash.js
@@ -1,30 +1,33 @@
 //flash.js: middleware
 
+const { lg } = require("@jowe81/lg");
+
 //Super simple flash messages
 const flash = (req, res, next) => {
-  console.log("Init middleware");
-
+  lg(`Registering middleware (flash)`,'-Flash');
   return (req, res, next) => {
-    console.log("First flash request");
 
-    const messages = [];
+    //Store flash messages in this closure
+    const _messages = [];
 
     //If already initialized, call the next function
     if (req.flash) {
       return next();
     }
+
     //Init the middleware.
     //Pass a message to add it. Omit the argument to retrieve the messages array
     if (!req.flash) {
       req.flash = (message) => {
         if (message) {
-          messages.push(message);
+          _messages.push(message);
         } else {
-          return messages;
+          return _messages;
         }
       };
       return next();
     }
+    
   };
 };
 
