d35e2c5ae27e22a61081e35cb5e2d524a7b76018
Johannes Weber <johannes@drweber.de>
2021-11-25 13:37:55 -0800

    add flash message for /urls/new

--- constants.js
+++ constants.js
@@ -12,11 +12,19 @@ const constants = {
   SALT_ROUNDS: 10,
 
   FLASH_MESSAGES: {
+
+    //Messages related to login/logout
     LOGIN: {
       WELCOME: "Welcome! TinyApp lets you shorten and store URLs. Register or login to begin.",
       WELCOME_BACK: "Welcome back! Login to view and store your URLs.",
       GOODBYE: "You logged out successfully. Goodbye!",
     },
+
+    //Messages related to creating new short-URLs
+    NEW_URL: {
+      NOTHING_ENTERED: "Looks like you did not enter anything - please enter a valid URL to shorten.",
+      BAD_URL: "You entered an invalid URL. Please try again.",
+    }
     
   }
 };
