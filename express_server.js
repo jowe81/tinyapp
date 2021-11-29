46374b07b032da7393ae549c62cfbde36ff63361
Johannes Weber <johannes@drweber.de>
2021-11-28 20:25:12 -0800

    support constants.PERSIST_TO_FILE and cmdline arg -init-from-file

--- express_server.js
+++ express_server.js
@@ -1,15 +1,15 @@
 //express_server.js: TinyApp entry point
 
-//***** Init logging *****************************
+//***** Init logging ***************************************
 const { lg, prefix } = require("@jowe81/lg");
 prefix.set("Server");
 lg(`TinyApp server starting...`);
 
-//****** Init constants and database defaults ****
+//****** Init constants and database defaults **************
 const constants = require("./constants");
 const database = require("./database");
 
-//****** ExpressJS setup *************************
+//****** ExpressJS setup ***********************************
 const express = require("express");
 const app = express();
 
@@ -36,38 +36,54 @@ app.set('view engine','ejs');
 const { registerRoutes } = require("./routes");
 registerRoutes(app);
 
-//****** Keyboard listeners **********************
+//****** Keyboard listeners for shutdown commands **********
 const input = require("./input");
+
+//A little helper to keep this section DRY...
+const exit = () => {
+  lg(`Thanks for using TinyApp - Goodbye!`);
+  process.exit();
+};
 
 //Catch termination requests (q/CTRL+C)
 input.onTerminate('q', () => {
-  lg(`Shutting down...`);
-  database.persistToFile().catch(() => {
-    lg(`Unable to save database to file - quitting anyway...`);
-  }).finally(() => {
-    lg(`Thanks for using TinyApp - Goodbye!`);
-    process.exit();
-  });
+  if (constants.PERSIST_TO_FILE) {
+    database.persistToFile().catch(() => {
+      lg(`Unable to save database to file - quitting anyway...`);
+    }).finally(exit);
+  } else exit();
 });
 
 //Terminate without saving database to file
-input.on('x', () => {
-  lg(`Thanks for using TinyApp - Goodbye!`);
-  process.exit();
-});
+input.on('x', exit);
 
-//****** Start TinyApp Server ********************
+//****** Start TinyApp Server ******************************
 
 //Get port from command line or fall back to constants.js
 const args = require("./args");
 const port = args.port > 0 ? args.port : constants.PORT;
 
-//Restore database from file and start listening
-//- In case the promise rejects, default data is used
-database.initFromFile().catch(() => {
-  lg(`Database init from file failed; using default records`);
-}).finally(()=>{
+const listen = () => {
   app.listen(port, () => {
     lg(`Listening on port ${port}`);
   });
-});
+};
+
+//Determine whether to init db from file: cmdline arg trumps constants.js
+let initFromFile = constants.PERSIST_TO_FILE;
+if (args["init-from-file"]) {
+  initFromFile = args["init-from-file"] === 'true' ? true : false;
+}
+
+if (initFromFile) {
+  //Restore database from file and start listening
+  //- In case the promise rejects, default data is used
+  database.initFromFile().catch(() => {
+    lg(`Database init from file failed; using default records`);
+  }).finally(()=>{
+    listen();
+  });
+} else {
+  //Start without reading data from file (fall back to defaults)
+  listen();
+}
