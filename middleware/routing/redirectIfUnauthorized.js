35654f3c8cad596f378c4a14cb5e78f19ef9d1ad
Johannes Weber <johannes@drweber.de>
2021-11-28 18:50:51 -0800

    add console output

--- middleware/routing/redirectIfUnauthorized.js
+++ middleware/routing/redirectIfUnauthorized.js
@@ -1,10 +1,14 @@
 //redirectIfUnauthorized.js: routing-middleware
 // - redirect to /login if user is not logged in
 
+const lg = require("@jowe81/lg");
+const logPrefix = "-redir";
+
 const database = require("../../database");
 
 const redirectIfUnauthorized = (req, res, next) => {
   if (!database.getUserByID(req.session.getUserID())) {
+    lg(`Redirecting unauthorized request to protected page to /login`, logPrefix);
     return res.redirect("/login");
   }
   next();
