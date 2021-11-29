45644d745b599833b181bf8f28d08a3e02802558
Johannes Weber <johannes@drweber.de>
2021-11-28 17:59:20 -0800

    track selected paths instead of everything

--- middleware/analytics.js
+++ middleware/analytics.js
@@ -5,17 +5,41 @@ const { lg } = require("@jowe81/lg");
 
 const logPrefix = '-Altcs';
 
+const _pathsToTrack = [];
+
+//True if reqUrl matches any of the paths that we are tracking
+const haveMatchingPath = (reqUrl) => {
+  for (const path of _pathsToTrack) {
+    if (reqUrl.substr(0,path.length) === path) {
+      return true;
+    }
+  }
+  return false;
+};
+
+//Register a path to track
+const addPath = (path) => {
+  if (!_pathsToTrack.includes(path)) {
+    _pathsToTrack.push(path);
+  }
+};
+
 //Super simple analytics: register each visit to each path
 const analytics = (req, res, next) => {
   lg(`Registering middleware (analytics)`, logPrefix);
 
   return (req, res, next) => {
-    //Register this visit to this path
-    lg(`Registering visit to ${req.url}`, logPrefix);
-    database.registerVisit(req.url, req.sessionID);
+    //Register this visit if it is to a path that we are tracking
+    if (haveMatchingPath(req.url)) {
+      lg(`Registering visit to ${req.url}`, logPrefix);
+      database.registerVisit(req.url, req.sessionID);
+    }
     next();
   };
 
 };
 
-module.exports = analytics;
+module.exports = {
+  addPath,
+  analytics,
+};
