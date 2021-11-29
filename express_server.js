5d64ea85483008d5529a2e372e1b5caf0012ec16
Johannes Weber <johannes@drweber.de>
2021-11-28 17:59:32 -0800

    reorder code for legibility

--- express_server.js
+++ express_server.js
@@ -1,27 +1,18 @@
 //express_server.js: TinyApp entry point
 
-const constants = require("./constants");
-const args = require("./args");
-const { registerRoutes } = require("./routes");
-
-//Init database
-const database = require("./database");
-database.initFromFile();
-
-const input = require("./input");
-input.onTerminate('q', () => {
-  lg(`Shutting down`);
-  database.persistToFile(() => {
-    lg(`Thanks for using TinyApp - Goodbye!`);
-    process.exit();
-  });
-});
-
-//Setup logging
+//***** Init logging *****************************
 const { lg, prefix } = require("@jowe81/lg");
 prefix.set("Server");
 lg(`TinyApp server starting...`);
 
+
+//***** Init constants and database **************
+const constants = require("./constants");
+const database = require("./database");
+database.initFromFile();
+
+
+//****** ExpressJS setup *************************
 const express = require("express");
 const app = express();
 
@@ -33,20 +24,39 @@ const cookieParser = require("cookie-parser");
 app.use(cookieParser());
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended:true}));
-const sessions = require("./middleware/sessions");
+const sessions = require("./middleware/sessions"); //My own basic sessions
 app.use(sessions());
-const analytics = require("./middleware/analytics");
-app.use(analytics());
-const flash = require("./middleware/flash");
+const analytics = require("./middleware/analytics"); //My own basic analytics
+analytics.addPath("/u/"); //Only track visits of short urls, not the whole app
+app.use(analytics.analytics());
+const flash = require("./middleware/flash"); //My own basic flash messages
 app.use(flash());
 
 //Use EJS templating engine
 app.set('view engine','ejs');
 
 //Register all route definitions in routes.js
+const { registerRoutes } = require("./routes");
 registerRoutes(app);
 
+
+//****** Keyboard listeners **********************
+const input = require("./input");
+
+//Catch termination requests (q/CTRL+C)
+input.onTerminate('q', () => {
+  lg(`Shutting down`);
+  database.persistToFile(() => {
+    lg(`Thanks for using TinyApp - Goodbye!`);
+    process.exit();
+  });
+});
+
+
+//****** Start TinyApp Server ********************
+
 //Get port from command line or fall back to constants.js
+const args = require("./args");
 const port = args.port > 0 ? args.port : constants.PORT;
 
 app.listen(port, () => {
